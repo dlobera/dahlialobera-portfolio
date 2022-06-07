002af370a8971a5644f0822965691ef876e2608d
dlobera <delobera@gmail.com>
2022-06-07 18:21:32 -0500

    function bootstrap dropdown

--- data/work.js
+++ data/work.js
@@ -1,9 +1,13 @@
 const projects = [
   {
     title: 'Tic-Tac-Toe',
-    image: '',
+    image: '/assets/images/IMG_9275 2.jpg',
     description: '2 user game of tic tac toe.',
-    githubLink: '',
-    deploymentLink: ''
+    githubLink: 'https://github.com/dlobera/ttt-weekend.git',
+    deploymentLink: 'https://antique-tictactoe.netlify.app/'
   }
 ]
+
+export {
+  projects
+}
