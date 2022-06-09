79a6bf55dd533309386afc834ef8304efbfcf30f
dlobera <delobera@gmail.com>
2022-06-08 23:39:02 -0500

    add contact icons

--- data/work.js
+++ data/work.js
@@ -1,13 +1,13 @@
-const projects = [
-  {
-    title: 'Tic-Tac-Toe',
-    image: '/assets/images/IMG_9275 2.jpg',
-    description: '2 user game of tic tac toe.',
-    githubLink: 'https://github.com/dlobera/ttt-weekend.git',
-    deploymentLink: 'https://antique-tictactoe.netlify.app/'
-  }
-]
+// const projects = [
+//   {
+//     title: 'Tic-Tac-Toe',
+//     image: '/assets/images/IMG_9275 2.jpg',
+//     description: '2 user game of tic tac toe.',
+//     githubLink: 'https://github.com/dlobera/ttt-weekend.git',
+//     deploymentLink: 'https://antique-tictactoe.netlify.app/'
+//   }
+// ]
 
-export {
-  projects
-}
+// export {
+//   projects
+// }
