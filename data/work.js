920ab864171011788c542e2980c478d50171b397
dlobera <delobera@gmail.com>
2022-07-01 03:51:17 -0500

    add project

--- data/work.js
+++ data/work.js
@@ -7,11 +7,11 @@ const projects = [
     deploymentLink: 'https://antique-tictactoe.netlify.app/',
   },
   {
-    title: 'More to Come',
-    image: './assets/comingsoon.png',
-    description: '....',
-    githubLink: 'https://github.com/dlobera/ttt-weekend.git',
-    deploymentLink: 'https://antique-tictactoe.netlify.app/',
+    title: 'kitchen collective',
+    image: './assets/kitchen.png',
+    description: 'A collection of recipes to share between friends and family.',
+    githubLink: 'https://github.com/dlobera/kitchen-collective',
+    deploymentLink: 'https://kitchen-collective.herokuapp.com//',
   },
 ]
 
