375304bb184a9e0d90ac2c57d6696247b5d4f047
dlobera <delobera@gmail.com>
2022-09-15 16:03:45 -0500

    update skills slideshow

--- data/skills.js
+++ data/skills.js
@@ -11,6 +11,22 @@ const skills = [
     title:'Javascript',
     image:'./assets/js.png',
   },
+  {
+    title: 'Python',
+    image:'./assets/python.png',
+  },
+  {
+    title:'Django',
+    image:'./assets/django.png',
+  },
+  {
+    title:'PostgreSQL',
+    image:'./assets/postgre.png',
+  },
+  {
+    title:'MongoDB',
+    image:'./assets/mongo.png'
+  }
 ]
 
 export {
